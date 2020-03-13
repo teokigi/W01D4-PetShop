d96b9919113c0b5ebeffd38ecfc2d794331f1ffd
Tony Tang <teokigi@hotmail.com>
2020-03-13 14:21:56 +0000

    add function returns shop cash

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,4 +1,8 @@
 #function returns petshop name
-def pet_shop_name(shop_name)
-    return shop_name[:name]
+def pet_shop_name(shop)
+    return shop[:name]
+end
+#function returns shops total cash value
+def total_cash(shop)
+    return shop.dig(:admin, :total_cash)
 end
